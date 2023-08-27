425fb60ce44bf9abb2eb306dcb651105c0af2027
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-08-27 09:52:02 +0700

    change password

--- src/controllers/web.controller.js
+++ src/controllers/web.controller.js
@@ -1,3 +1,6 @@
+const User = require("./../models/user.model");
+const bcrypt = require("bcryptjs");
+const gmail = require("./../mails/gmail");
 exports.home = (req,res) =>{
     // res.send("Hello T2207A");
     var className = "T2207A";
@@ -11,6 +14,38 @@ exports.home = (req,res) =>{
         students: students
     });
 }
+exports.change = (req, res) => {
+    res.render("change");
+}
+
+exports.postChangePassword = async (req, res) => {
+    try {
+        const {oldPassword, newPassword} = req.body;
+        const user = await User.findOne({email: req.session.auth.email});
+        if (!user) {
+            return res.send("Khong tim thay nguoi dung");
+        }
+        const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!isPasswordMatch) {
+            return res.send("Mat khau cu khong dung");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+        user.password = hashedNewPassword;
+        await user.save();
+        gmail.sendMail({
+            from: "Admin NodeJS",
+            to: user.email,  
+            subject: "Warning Changed Password",
+            html: "<h1>Mau khau cua ban da duoc thay doi</h1>"  
+        });
+        res.redirect("/"); 
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+
 exports.about = (req, res) =>{
     // res.send("About US");
     res.render("about")
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
 const controller = require("./../controllers/web.controller");
 
+// kiem tra dang nhap
+function requireLogin(req, res, next) {
+    if (req.session.auth && req.session.auth.email) {
+        next();
+    } else {
+        res.redirect("/auth/login");
+    }
+}
+
 router.get("/",controller.home)
+
+router.get("/change", requireLogin, controller.change);
+router.post("/change", requireLogin, controller.postChangePassword);
 
 router.get("/about",controller.about)
 
